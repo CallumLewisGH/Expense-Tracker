685771908b316354811a39adfe3fcd5c609d4fb4
Callum Lewis <callumlewis852@gmail.com>
2024-08-05 22:36:57 +0100

    Adds transaction list

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,14 +1,16 @@
 <template>
     <h3>History</h3>
         <ul id="list" class="list">
-            <li class="minus">
-                Cash <span>-£400</span>
-                <button class="delete-btn">x</button>
-            </li>
-
-            <li class="plus">
-                Wage <span>£400</span>
+            <li v-for="transaction in transactions" :key="transactions.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+               {{transaction.text}} <span>£{{transaction.amount}}</span>
                 <button class="delete-btn">x</button>
             </li>
         </ul>
 </template>
+
+<script setup>
+    const transactions=
+    [{id:1, text: "Salary", amount: 300},
+    {id:2, text: "Date", amount: -100}
+    ];
+</script>
